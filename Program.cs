faf9b16d0422ea09357145069675b5b355c5f150
Yaritza <yaritzamicaelleb@gmail.com>
2021-05-28 21:49:39 -0300

    Tarefa

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace SequenciaPares
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int contador=0, numero;
+
+            Console.Write("Números pares entre 0 e: ");
+            numero = int.Parse(Console.ReadLine());
+
+            while (contador <= numero)
+            {
+                Console.Write($"{contador} ");
+                contador= contador +2;
+            }   
+        }
+    }
+}
